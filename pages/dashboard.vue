2651cf0a775655162a2dc49bbccc06a816d2e97a
takashi.suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-02-09 21:51:48 +0900

    add auth ui

---
+++ pages/dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <amplify-authenticator>
+    <h1>dashboard</h1>
+    <p>hello, {{ name }}</p>
+  </amplify-authenticator>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Auth } from 'aws-amplify'
+
+export default Vue.extend({
+  data() {
+    return { name: Auth.userAttributes.name }
+  },
+})
+</script>
